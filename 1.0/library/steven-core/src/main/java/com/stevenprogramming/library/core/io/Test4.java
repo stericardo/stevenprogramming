de9c856bdd25bbe0a8778ab855ac77d13a456896
Steven Mendez <steven.mendez@accenture.com>
2017-11-28 22:04:39 -0600

    Steven Mendez AEM 6.3 Archetype and more

---
+++ 1.0/library/steven-core/src/main/java/com/stevenprogramming/library/core/io/Test4.java
@@ -0,0 +1,69 @@
+package com.stevenprogramming.library.core.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author steven.mendez
+ * @since Nov 10 2017
+ * @version 1.0
+ */
+public class ReadFileFromResources {
+
+    public InputStream readFile(String fileName) {
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream(fileName);
+        return is;
+    }
+
+    public String getStringFromInputStream(InputStream is) {
+        String result = new BufferedReader(new InputStreamReader(is))
+                .lines().collect(Collectors.joining("\n"));
+        return result;
+    }
+
+    public String getStringFromFile(String fileName) {
+        return getStringFromInputStream(readFile(fileName));
+    }
+
+    public String getStringParallelFromFile(String fileName) {
+        String result = new BufferedReader(new InputStreamReader(readFile(fileName)))
+                .lines().collect(Collectors.joining("\n"));
+        return result;
+    }
+
+    public StringBuilder getStringBuilderFromFile(String fileName, String encoding) {
+        final StringBuilder out = new StringBuilder();
+        try {
+            final int bufferSize = 1024;
+            final char[] buffer = new char[bufferSize];
+            Reader in = new InputStreamReader(readFile(fileName), encoding);
+            for (;;) {
+                int rsz = in.read(buffer, 0, buffer.length);
+                if (rsz < 0) {
+                    break;
+                }
+                out.append(buffer, 0, rsz);
+            }
+
+        } catch (UnsupportedEncodingException ignored) {
+            System.out.println(UnsupportedEncodingException.class.getName() + "\t" +ignored.toString());
+        } catch (IOException ignored) {
+            System.out.println(IOException.class.getName() + "\t" +ignored.toString());
+        }
+        return out;
+    }
+
+    public static void main(String[] args) {
+        ReadFileFromResources readFileFromResources = new ReadFileFromResources();
+        System.out.println(readFileFromResources.getStringFromFile("read.txt"));
+        System.out.println(readFileFromResources.getStringParallelFromFile("read.txt"));
+        System.out.println(readFileFromResources.getStringBuilderFromFile("read.txt", "UTF-8"));
+        
+    }
+}
