1d20778c1ceebf2eccfd04fbc87712e4ad17f79b
unknown <ricardostev@gmail.com>
2019-06-11 06:00:19 -0600

    Adding Model

---
+++ 1.0/library/steven-liquidbase/src/main/java/com/stevenprogramming/liquibase/model/Person.java
@@ -0,0 +1,62 @@
+package com.stevenprogramming.liquibase.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Steven Ricardo Mendez Brenes
+ */
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
+    
+    private String firstName;
+    private String lastName;
+    private char state;
+    
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+ 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} if (!lastName.equals(other.lastName))
+			return false;
+		
+		return true;
+	}
+
+}
