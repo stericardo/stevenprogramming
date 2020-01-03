e83c8e6befe07a47c48a8004c165e25e4dd26b5b
Steven Ricardo Mendez Brenes <smendez@snapfinance.com>
2020-01-03 17:42:35 -0600

    Java11

--- java11/modules/mymodapp/compile.sh
+++ java11/modules/mymodapp/compile.sh
@@ -1,12 +1,14 @@
-../../../../software/jdk-11.0.3/bin/java -version
+echo "JAVA11 ---- PATH:::" $JAVA11_HOME
+
+$JAVA11_HOME/bin/java -version
 
 #Functions
-../../../../software/jdk-11.0.3/bin/javac -d result/appfuncs appsrc/appfuncs/appfuncs/simplefuncs/SimpleMathFuncs.java
-../../../../software/jdk-11.0.3/bin/javac -d result/appfuncs appsrc/appfuncs/module-info.java
+$JAVA11_HOME/bin/javac -d result/appfuncs appsrc/appfuncs/appfuncs/simplefuncs/SimpleMathFuncs.java
+$JAVA11_HOME/bin/javac -d result/appfuncs appsrc/appfuncs/module-info.java
 
 #App testing
-../../../../software/jdk-11.0.3/bin/javac --module-path result -d result/appstart appsrc/appstart/module-info.java
-../../../../software/jdk-11.0.3/bin/javac --module-path result -d result/appstart appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
+$JAVA11_HOME/bin/javac --module-path result -d result/appstart appsrc/appstart/module-info.java
+$JAVA11_HOME/bin/javac --module-path result -d result/appstart appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
 
 #Execute application
-../../../../software/jdk-11.0.3/bin/java --module-path result -m appstart/appstart.mymodappdemo.MyModAppDemo
+$JAVA11_HOME/bin/java --module-path result -m appstart/appstart.mymodappdemo.MyModAppDemo
