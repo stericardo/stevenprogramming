dcfabf4db0e5021f3d3dc20b030d3cfe973bcffb
Steven Ricardo Mendez Brenes <smendez@ice-consultants.com>
2020-03-18 19:06:38 -0600

    Prac

--- 1.0/library/steven-core/src/main/java/com/stevenprogramming/library/core/practices/ArrayTestRecursive.java
+++ 1.0/library/steven-core/src/main/java/com/stevenprogramming/library/core/practices/ArrayTestRecursive.java
@@ -18,6 +18,59 @@ public class ArrayTestRecursive{
 		}
 	}
 
+	public static boolean isParent(int[][] vector, int value, int init){
+		for(int cont = init; cont < vector.length; cont++){
+			if (vector[cont][0] == value || vector[cont][1]  == value ){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static Node getNode2(Node nodeParent, int parent, int[][] vector, int init){
+		//int cont = init;
+		boolean found = false;
+		System.out.printf("%nStart parent: " + parent + " Node Parent " +nodeParent);
+		Node newParent= null;
+		for(int cont = init; cont < vector.length; cont++){
+			//System.out.printf("%nParent %d, cont %d ", parent, cont);
+			//System.out.printf("%n    BEFORE FOUND Parent %d, cont %d, vector[0][1]=%d, vector[1][0]=%d ",
+			//parent, cont, vector[cont][0], vector[cont][1]);
+			if (vector[cont][0] == parent || vector[cont][1]  == parent ){
+				found = true;
+				System.out.printf("%n    **********   ******Parent %d, cont %d, vector[0][1]=%d, vector[1][0]=%d ", parent, cont, vector[cont][0], vector[cont][1]);
+				int childValue = getChild(parent, vector, cont);
+
+				if(isParent(vector, childValue,cont+1)){
+					newParent= new Parent( String.valueOf( childValue ) );
+				} else{
+					newParent= nodeParent;
+				}
+				//Node newParent= new Parent( childValue );
+				//Node child = new Child( childValue );
+				newParent = getNode2(newParent, childValue, vector, cont+1);
+				nodeParent.add( newParent );
+				//System.out.println("\n\n\t\t\t ####### Return Node Parent:: " + nodeParent + "\n New " + newParent);
+				for (Node c: ((Parent)nodeParent).children){
+						//			System.out.print(" *Print ParentParemeter " + parent+ " Node " + c);
+				}
+			}
+		}
+		if (found){
+			//System.out.println("\n----Returning Parent found " + parent + " Node Parent " + nodeParent );
+			return nodeParent;
+		} else {
+			Node child = new Child( String.valueOf(parent) );
+			//nodeParent.add(child);
+			//System.out.println("\n---------Returning Child Part Parent "+ parent+ " newParent> " + nodeParent );
+			for (Node c: ((Parent)nodeParent).children){
+			//		System.out.print(c);
+			}
+			return child;
+		}
+
+	}
+
 	public static Node getNode(int parent, int[][] vector, int init){
 		boolean isParent = false;
 		Node nodeParent= null;
@@ -30,15 +83,17 @@ public class ArrayTestRecursive{
 				isParent = true;
 				if( nodeParent== null) {
 					if( isZeroParent(parent, vector, cont) ){
+						System.out.printf("%nCreating isZeroParent %d cont %d Vector[%d][%d]",  parent, cont, vector[cont][0], vector[cont][1]);
 						nodeParent= new Parent( String.valueOf ( vector[cont][0] ) );
 					} else {
+						//System.out.printf("%nCreating NOT isZeroParent %d cont %d vector[0][1]=%d, vector[1][0]=%d",  parent, cont, vector[cont][0], vector[cont][1]);
 						nodeParent= new Parent( String.valueOf ( vector[cont][1] ) );
 					}
 				}
 
 				childInt = getChild(parent, vector, cont);
 				Node child = getNode(childInt, vector, init + 1);
-				System.out.printf("%nAddint %d to parent: %d, contador: %d V[%d][%d] ", childInt, parent, cont, vector[cont][0], vector[cont][1] );
+				//System.out.printf("%nAddint %d to parent: %d, contador: %d vector[0][1]=%d, vector[1][0]=%d ", childInt, parent, cont, vector[cont][0], vector[cont][1] );
 				nodeParent.add(child);
 			}
 		}
@@ -54,7 +109,8 @@ public class ArrayTestRecursive{
 
 		int[][] v2= {{1,2}, {1,3}, {1,6}, {4,2}, {2,5}, {7,6}, {6,8}, {8,9}, {8,10}};
 
-		Node node = getNode(1, v2, 0);
+		Node nodeParent = new Parent("1");
+		Node node = getNode2(nodeParent, 1, v2, 0);
 		System.out.println(node);
 	}
 
@@ -65,7 +121,7 @@ public class ArrayTestRecursive{
 
 	static class Parent extends Node{
 		String name;
-		List<Node> children = new ArrayList<>();
+		public List<Node> children = new ArrayList<>();
 		public Parent(String name){
 			this.name = name;
 		}
@@ -77,9 +133,9 @@ public class ArrayTestRecursive{
 		public String toString(){
 			StringBuilder sb = new StringBuilder();
 			for (Node c: children){
-			//	sb.append(children);
+				sb.append(children);
 			}
-			return "\n\nName " + name + " Size children " +children.size() + sb.toString();
+			return "\nNode-toString\t " + name + " Size children " +children.size() + sb.toString();
 		}
 	}
 
